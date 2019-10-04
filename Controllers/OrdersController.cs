f96f80263df9cc8afd8b375dc2e5cde7080073f5
JJ Garlitos <acoustic_ninja@hotmail.com>
2019-10-03 20:46:31 -0600

    Added OrdersController and OrdersService w/ basic functions

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ZenStore.Models;
+using ZenStore.Services;
+
+namespace ZenStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly OrdersService _os;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Order>> Get()
+        {
+            try
+            {
+                return Ok(_os.GetOrders());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Order> GetById(string id)
+        {
+            try
+            {
+                return Ok(_os.GetOrderById(id));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<Order> Create([FromBody] Order orderData)
+        {
+            try
+            {
+                Order order = _os.AddOrder(orderData);
+                return Created("api/orders/" + order.Id, order);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Order> Edit(string id, [FromBody] Order orderData)
+        {
+            try
+            {
+                orderData.Id = id;
+                return Ok(_os.EditOrder(orderData));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        public OrdersController(OrdersService os)
+        {
+            _os = os;
+        }
+    }
+}
---
+++ Services/OrdersService.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ZenStore.Data;
+using ZenStore.Models;
+
+namespace ZenStore.Services
+{
+    public class OrdersService
+    {
+        private readonly OrdersRepository _repo;
+
+        public List<Order> GetOrders()
+        {
+            return _repo.GetAll().ToList();
+        }
+
+        public Order GetOrderById(string id)
+        {
+            var order = _repo.GetById(id);
+            if (order == null) { throw new Exception("You're taking empty mind too far. This order doesn't even exist."); }
+
+            return order;
+        }
+
+        //NOTE Work on this
+        public Order AddOrder(Order orderData)
+        {
+            orderData.Id = Guid.NewGuid().ToString();
+            _repo.Create(orderData);
+
+            return orderData;
+        }
+
+        //NOTE Work on this
+        public Order EditOrder(Order orderData)
+        {
+            var order = _repo.GetById(orderData.Id);
+            if (order == null) { throw new Exception("You're taking empty mind too far. This order doesn't even exist."); }
+            order.Name = orderData.Name;
+            // order.Description = orderData.Description;
+
+            return _repo.Edit(order);
+        }
+
+        public OrdersService(OrdersRepository repo)
+        {
+            _repo = repo;
+        }
+    }
+}
